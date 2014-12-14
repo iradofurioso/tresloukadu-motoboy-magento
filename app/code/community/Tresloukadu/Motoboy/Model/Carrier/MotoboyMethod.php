3dbaec0fa1f2d3421a21a490d5c490b0e449eabe
Bruno Oliveira <321.bruno@gmail.com>
2014-12-14 13:42:34 -0200

    Bairros no banco de dados
    Os bairros agora está sendo gravado no banco de dados de forma simples
    pelo painel de configuração, onde foi criado um textarea com as lista de
    bairros com preços, ao instalar o módulo tem um descritivo de como
    cadastrar. Apartir disto o código está preparado para receber essas
    informações e exibir de forma correta para o cliente no checkout. Espero
    ter ajudado.

--- app/code/community/Tresloukadu/Motoboy/Model/Carrier/MotoboyMethod.php
+++ app/code/community/Tresloukadu/Motoboy/Model/Carrier/MotoboyMethod.php
@@ -86,99 +86,46 @@ class Tresloukadu_Motoboy_Model_Carrier_MotoboyMethod extends Mage_Shipping_Mode
 		  * @todo Precisa trazer estes dados do banco de dados ou de um arquivo xml. O usuário deve 
 		  * fornecer estes dados no admin e depois armazenar no banco/xml.
 		 */
-		$bairros = array(
-				'Armacao'=>'50.00',
-				'Area Industrial de sao Jose'=>'17.00',
-				'Agronomica'=>'25.00',
-				'Alto Aririu'=>'25.00',
-				'Aeroporto'=>'35.00',
-				'Av. das Torres'=>'20.00',
-				'Abraao'=>'17.00',
-				'Aririu'=>'25.00',
-				'Areias'=>'17.00',
-				'Area Ind. da Palhoca'=>'20.00',
-				'Barreiros'=>'17.00',
-				'Bela Vista'=>'15.00',
-				'Bela Vista da Palhoca'=>'20.00',
-				'Balneario do estreito'=>'17.00',
-				'Biguacu'=>'25.00',
-				'Barra da Lagoa'=>'40.00',
-				'Barra do Aririu'=>'25.00',
-				'Centro'=>'20.00',
-				'Corrego Grande'=>'30.00',
-				'Coloninha'=>'17.00',
-				'Campeche'=>'30.00',
-				'Cacupe'=>'35.00',
-				'Canasvieiras'=>'50.00',
-				'Ceniro Martins'=>'12.00',
-				'Colonia Santana'=>'25.00',
-				'Costeira'=>'25.00',
-				'Caminho Novo'=>'25.00',
-				'Centro de Sao Jose'=>'15.00',
-				'Campinas'=>'15.00',
-				'Carvoeira'=>'25.00',
-				'Capoeiras'=>'17.00',
-				'Coqueiros'=>'17.00',
-				'Cachoeira do bom Jesus'=>'50.00',
-				'Forquilinhas'=>'10.00',
-				'Forquilhas'=>'15.00',
-				'Floresta'=>'12.00',
-				'Fazenda do Max'=>'15.00',
-				'Ingleses'=>'50.00',
-				'Itacorobi'=>'25.00',
-				'jardim el Dourado'=>'15.00',
-				'Joao Paulo'=>'30.00',
-				'Jurere'=>'45.00',
-				'Janaina'=>'25.00',
-				'Kobrassol'=>'15.00',
-				'Lisboa'=>'10.00',
-				'Palhoca(centro)'=>'17.00',
-				'Ponta de baixo'=>'15.00',
-				'Ponte do Imaruim'=>'15.00',
-				'Pantanal'=>'25.00',
-				'Rocado'=>'12.00',
-				'Rio Tavares'=>'30.00',
-				'Ratones'=>'45.00',
-				'Serraria'=>'20.00',
-				'Sertao do Imaruim'=>'15.00',
-				'Saco grande'=>'30.00',
-				'Santa Monica'=>'30.00',
-				'Santo Antonio de Lisboa'=>'35.00',
-				'Sao Sebastiao'=>'30.00',
-				'Sao Pedro de Alcantara'=>'40.00',
-				'Santo Amaro'=>'35.00',
-				'Tapera'=>'45.00',
-				'Trindade'=>'25.00',
-				'Vila Formosa'=>'12.00',
-				'Zanelato'=>'20.00');
-		
+		$bairros = array();
+		$bairros_data = $this->getConfigData('bairros');		 
+		if($bairros_data){
+			$aux = explode("\n", str_replace("\r", "", $bairros_data));
+			$new_bairros = array();
+			foreach($aux as $item){
+				$aux_item = explode("#", $item);
+				$valor = str_replace(",", ".", $aux_item[1]);
+				$valor = str_replace(" ", ".", $valor);
+				$new_bairros[trim($aux_item[0])] = $valor;
+			}
+			$bairros = $new_bairros;
+		} 
+				
 		$result = Mage::getModel('shipping/rate_result');
 		
-		foreach($bairros as $chave => $valor )
-		{
-			$shippingPrice = $valor; 
-			  
-			$shippingPrice = $this->getFinalPriceWithHandlingFee($shippingPrice);
-			
-			$method = Mage::getModel('shipping/rate_result_method');
-			
-			$method->setCarrier($this->_code);
-			$method->setCarrierTitle($this->getConfigData('title'));
-			$method->setMethod($chave);
-			$method->setMethodTitle($chave); 
-			if ($request->getFreeShipping() === true || $request->getPackageQty() == $this->getFreeBoxes()) {
-				$shippingPrice = '0.00';
+		if(count($bairros)){
+			foreach($bairros as $chave => $valor )
+			{
+				$shippingPrice = $valor; 
+				  
+				$shippingPrice = $this->getFinalPriceWithHandlingFee($shippingPrice);
+				
+				$method = Mage::getModel('shipping/rate_result_method');
+				
+				$method->setCarrier($this->_code);
+				$method->setCarrierTitle($this->getConfigData('title'));
+				$method->setMethod($chave);
+				$method->setMethodTitle($chave); 
+				if ($request->getFreeShipping() === true || $request->getPackageQty() == $this->getFreeBoxes()) {
+					$shippingPrice = '0.00';
+				}
+				$method->setPrice($shippingPrice);
+				$method->setCost($shippingPrice);
+				
+				$result->append($method);
 			}
-			$method->setPrice($shippingPrice);
-			$method->setCost($shippingPrice);
-			
-			$result->append($method);
-		}
-		
+		}	
 		return $result;
 	}
-	
-	
 	
 	public function getAllowedMethods()
 	{
